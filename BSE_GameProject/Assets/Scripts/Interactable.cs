17b309f74b636b7ffbb7091f8d50a4006e9402e1
RESUHT <lucas.manssen@mds.ac.nz>
2021-11-16 17:38:48 +1300

    WIP - Items, inventory scripts

---
+++ BSE_GameProject/Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    bool isFocused = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        //this method is meant to be overridden
+    }
+
+    private void Update()
+    {
+        if(isFocused && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocused = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocused = false;
+        player = null;
+        hasInteracted = false;
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
